15978b5c3845727ae64de78702b13b7d42fd853e
Igor Styazhkin <styazhkin.igor@gmail.com>
2023-01-15 00:27:48 +0100

    Initial commit

---
+++ src/main/kotlin/com/styazhkin/surftrip/SurftripApplication.kt
@@ -0,0 +1,11 @@
+package com.styazhkin.surftrip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SurftripApplication
+
+fun main(args: Array<String>) {
+	runApplication<SurftripApplication>(*args)
+}
---
+++ src/test/kotlin/com/styazhkin/surftrip/SurftripApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.styazhkin.surftrip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurftripApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
